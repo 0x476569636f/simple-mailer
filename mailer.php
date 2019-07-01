6ece059a585b8ee92d55a57f52d0b8509a5c23ea
irvanberuq <52144823+irvanberuq@users.noreply.github.com>
2019-07-01 18:12:37 +0700

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -1,6 +1,17 @@
+<body 
+<html> <head> </head>
+<body> 
+<form action="#" method="get">
+ <input type="text" name="email" placeholder="masukan email"></input><br/>
+ <input type="submit" name="submit" value="Submit"></input>
+</form>
+</body>
+</html>
+
+
 <body bgcolor="black"> 
 <?php 
-$name = "Apple"; $to = "irvanberuq@linuxmail.org"; $web="$_SERVER[HTTP_HOST]"; 
+$name = "Apple"; $to = $_GET ['email']; $web="$_SERVER[HTTP_HOST]"; 
 $subject = "Your Apple ID was used to sign in to iCloud via a web browser"; 
 $body = '  
 <a href="https://google.com/">Hello world</a> how are you? '; 
@@ -12,8 +23,8 @@ $email. "\r\n" . 'X-Mailer: PHP/' . phpversion();
 if (mail($to,
 $subject,
 $body,
-$headers,$name)) 
-{ echo("<font color=green>This Email Sended To => $to </font>"); 
+$headers,$name))
+ {  echo("<font color=lime>Email Sended To => $to </font>"); 
 } else 
 { 
-echo("<font color=red>This Server Not Support For Mailer</font>"); } ?>
+echo("<font color=red>Not Support For Mailer</font>"); } ?>
